8c6c4ed739ee8912c7d6e8f1e1dd7c0ad58a9f8c
JonOuellette <jonathan.t.ouellette@gmail.com>
2022-08-10 14:27:50 -0700

    tie alert popping up on click - not switching player

--- connect4.js
+++ connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -26,23 +26,23 @@ function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.querySelector('#board')
   // TODO: add comment for this code
-  var top = document.createElement("tr");   //creates a table row element
+  const top = document.createElement("tr");   //creates a table row elements and assigns the name of top
   top.setAttribute("id", "column-top");     // sets an id attribute of column-top to the tr element top that was created
   top.addEventListener("click", handleClick);   //sets a click event listener that will execute the handleClick function
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  for (let x = 0; x < WIDTH; x++) {                 
+    const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code            the nested loop is used to creates the game board which is a 2 dimensional array
-  for (var y = 0; y < HEIGHT; y++) {          //creates a loop that iterates up to the value of height
+  // TODO: add comment for this code            //the nested loop is used to creates the game board which is a 2 dimensional array
+  for (let y = 0; y < HEIGHT; y++) {          //creates a loop that iterates up to the value of height
     const row = document.createElement("tr");   //createa table row element through each iteration of the loop and setting the value to row
-    for (var x = 0; x < WIDTH; x++) {           // creates a nested loop that iterates up to the value of height
+    for (let x = 0; x < WIDTH; x++) {           // creates a nested loop that iterates up to the value of height
       const cell = document.createElement("td");     // creates table data element through each iteration and setting value to cell
-      cell.setAttribute("id", `${y}-${x}`);         // sets an id attribute to the value of y-x
+      cell.setAttribute("id", `${y}-${x}`);         // sets an id attribute to the value of y-x for each table data element created
       row.append(cell);                             // appends the cell element
     }
     htmlBoard.append(row);                           //appends the row element
@@ -53,34 +53,40 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return board.filter((row) => row[x] === null).length -1
+     for(let y = (HEIGHT - 1); y >= 0; y--){
+      if (!board[y][x]){
+        return y; 
+      }
+     }
+     return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
-  const spot = document.quertySelector('#${y}-${x}')
-  const piece = document.createElement('div')
+  const piece = document.createElement("div");
   piece.classList.add("piece");
-  piece.classList.add('Player${currPlayer}')
-  spot.append(piece);
+  piece.classList.add(`p${currPlayer}`);
+  const cord = document.getElementById(`${y}-${x}`);
+  cord.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -88,6 +94,7 @@ function handleClick(evt) {
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
+  board[y][x] = currPlayer;
 
   // check for win
   if (checkForWin()) {
@@ -96,9 +103,13 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-
+  if (board.every(row => row.every(cell => cell))) {
+    return endGame('Tie!');
+  }
+  
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -120,13 +131,13 @@ function checkForWin() {
   }
 
   // TODO: read and understand this code. Add comments to help you.
-
+// this checks for each possible winning condition
   for (var y = 0; y < HEIGHT; y++) {
     for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];   // loops through the array matrix to verify if there are 4 in a row horizontally
+      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];     // loops through the array matrix to verify if there are 4 in a row vertically
+      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];    //loops through the array matrix to verify if there are 4 in a row going in a right diagonal
+      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];    //loops through the array matrix to verify if there are 4 in a row going in a left diagonal
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
